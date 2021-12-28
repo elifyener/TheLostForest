da594b64b5c3c1a36fbadb181441197752f5704d
Elif Yener <elif.yener@bil.omu.edu.tr>
2021-12-28 23:33:07 +0300

    checkpoint system.

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 
 public class CheckPoint : MonoBehaviour
 {
-    
+    public Death deathWall;
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.gameObject.CompareTag("CheckPoint"))
+        {
+            deathWall.checkPos = transform.position;
+        }
+    }
 }
